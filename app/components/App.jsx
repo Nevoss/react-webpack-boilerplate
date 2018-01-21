7a95f8c29be2cb0beeab31c7516d95f6c60f780b
Nevo Golan <nevos12@gmail.com>
2018-01-21 22:25:41 +0200

    bootstrap and main app component

---
+++ app/components/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container mt-4">
+        <h1 className="text-center">
+          Hey, there!
+        </h1>
+      </div>
+    )
+  }
+}
+
+export default App
--- app/index.js
+++ app/index.js
@@ -1,1 +1,6 @@
 import './styles/app.scss'
+import React from 'react' 
+import ReactDOM from 'react-dom'
+import App from './components/App.jsx'
+
+ReactDOM.render(<App />, document.getElementById('app'))
